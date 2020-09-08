9dc3e82ea0462fcf63f2b16f01fd4c578c35ce3e
Sirttas <3-Sirttas@shendai.rip>
2020-09-08 06:12:45 +0000

    2.2.2

--- src/main/java/sirttas/elementalcraft/ElementalCraft.java
+++ src/main/java/sirttas/elementalcraft/ElementalCraft.java
@@ -4,22 +4,20 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.client.event.ClientPlayerNetworkEvent;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.server.FMLServerStartedEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import sirttas.elementalcraft.block.tile.renderer.ECRenderers;
 import sirttas.elementalcraft.config.ECConfig;
-import sirttas.elementalcraft.entity.ECEntities;
 import sirttas.elementalcraft.item.pureore.PureOreHelper;
 import sirttas.elementalcraft.loot.function.ECLootFunctions;
 import sirttas.elementalcraft.network.message.MessageHandler;
+import sirttas.elementalcraft.network.proxy.ClientProxy;
+import sirttas.elementalcraft.network.proxy.IProxy;
 import sirttas.elementalcraft.world.ECFeatures;
 
 @Mod(ElementalCraft.MODID)
@@ -28,11 +26,14 @@ public class ElementalCraft {
 
 	public static final Logger T = LogManager.getLogger(MODID);
 
+	public static IProxy proxy = new IProxy() {
+	};
+
 	public ElementalCraft() {
+		DistExecutor.safeCallWhenOn(Dist.CLIENT, () -> () -> proxy = new ClientProxy()); // NOSONAR
+		proxy.registerHandlers();
 		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setupClient);
 		MinecraftForge.EVENT_BUS.addListener(this::setupServer);
-		MinecraftForge.EVENT_BUS.addListener(this::clientLoggin);
 	}
 
 	private void setup(FMLCommonSetupEvent event) {
@@ -43,15 +44,7 @@ public class ElementalCraft {
 		ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, ECConfig.SPEC);
 	}
 
-	@OnlyIn(Dist.CLIENT)
-	private void setupClient(FMLClientSetupEvent event) {
-		ECRenderers.initRenderLayouts();
-		ECEntities.registerRenderers();
-	}
 
-	private void clientLoggin(ClientPlayerNetworkEvent.LoggedInEvent event) {
-		PureOreHelper.generatePureOres(event.getPlayer().getEntityWorld().getRecipeManager());
-	}
 
 	private void setupServer(FMLServerStartedEvent event) {
 		PureOreHelper.generatePureOres(event.getServer().getRecipeManager());
