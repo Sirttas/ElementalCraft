b3cf90baff9c35e3cb14ed41bad0f0bc0cfb1198
Sirttas <calloch.gael@gmail.com>
2020-10-07 21:34:15 +0000

    2.3.3

--- src/main/java/sirttas/elementalcraft/network/proxy/ClientProxy.java
+++ src/main/java/sirttas/elementalcraft/network/proxy/ClientProxy.java
@@ -1,13 +1,11 @@
 package sirttas.elementalcraft.network.proxy;
 
-import net.minecraftforge.client.event.ClientPlayerNetworkEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import sirttas.elementalcraft.block.tile.renderer.ECRenderers;
 import sirttas.elementalcraft.entity.ECEntities;
 import sirttas.elementalcraft.item.ECItems;
-import sirttas.elementalcraft.item.pureore.PureOreHelper;
 import sirttas.elementalcraft.spell.SpellTickManager;
 
 public class ClientProxy implements IProxy {
@@ -17,16 +15,11 @@ public class ClientProxy implements IProxy {
 		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setupClient);
 		FMLJavaModLoadingContext.get().getModEventBus().addListener(ECItems::registerItemColors);
 
-		MinecraftForge.EVENT_BUS.addListener(this::clientLoggin);
 		MinecraftForge.EVENT_BUS.addListener(SpellTickManager::clientTick);
 	}
 
 	private void setupClient(FMLClientSetupEvent event) {
 		ECRenderers.initRenderLayouts();
 		ECEntities.registerRenderers();
-	}
-
-	private void clientLoggin(ClientPlayerNetworkEvent.LoggedInEvent event) {
-		PureOreHelper.generatePureOres(event.getPlayer().getEntityWorld().getRecipeManager());
 	}
 }
