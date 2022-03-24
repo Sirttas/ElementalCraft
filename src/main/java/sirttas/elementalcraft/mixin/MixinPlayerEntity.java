e64c1e86fc4a077180d2c98022b266fb522832cf
UnknownDev <10685816+iCry1337@users.noreply.github.com>
2022-03-24 16:39:55 +0100

    fix Startup Crash with SpongeForge 1.16.5 (#77)

--- src/main/java/sirttas/elementalcraft/mixin/MixinPlayerEntity.java
+++ src/main/java/sirttas/elementalcraft/mixin/MixinPlayerEntity.java
@@ -1,9 +1,8 @@
 package sirttas.elementalcraft.mixin;
 
+import net.minecraft.item.SwordItem;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.At.Shift;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 import net.minecraft.entity.Entity;
@@ -21,14 +20,18 @@ public abstract class MixinPlayerEntity extends LivingEntity {
 		super(type, worldIn);
 	}
 
-	@ModifyVariable(method = "attack(Lnet/minecraft/entity/Entity;)V", 
-			index = 10, 
-			at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/item/ItemStack;getItem()Lnet/minecraft/item/Item;", shift = Shift.AFTER))
-	public boolean attackTargetEntityWithCurrentItemSetFlag3(boolean flag3) {
-		if (isHoldinStaff()) {
-			return true;
-		}
-		return flag3;
+	@SuppressWarnings("rawtypes")
+	@Redirect(
+			method = "attack(Lnet/minecraft/entity/Entity;)V",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/item/ItemStack;getItem()Lnet/minecraft/item/Item;",
+					shift = At.Shift.AFTER
+			)
+	)
+
+	private boolean impl$onAttack(final Object item, final Class target) {
+		return item instanceof SwordItem || isHoldinStaff();
 	}
 	
 	@Redirect(method = "attack(Lnet/minecraft/entity/Entity;)V", 
@@ -43,5 +46,4 @@ public abstract class MixinPlayerEntity extends LivingEntity {
 	private boolean isHoldinStaff() {
 		return this.getMainHandItem().getItem() instanceof StaffItem;
 	}
-	
 }
