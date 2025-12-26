b3fd5f16a9383a05e9886ef3e6a37d44f5b491bc
Sirttas <calloch.gael@gmail.com>
2025-12-26 16:18:40 +0100

    cleanup test

--- src/test/java/sirttas/elementalcraft/block/shrine/upgrade/directional/RangeShrineUpgradeTemplates.java
+++ src/test/java/sirttas/elementalcraft/block/shrine/upgrade/directional/RangeShrineUpgradeTemplates.java
@@ -3,6 +3,7 @@ package sirttas.elementalcraft.block.shrine.upgrade.directional;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.neoforged.neoforge.common.util.Lazy;
+import net.neoforged.testframework.annotation.RegisterStructureTemplate;
 import net.neoforged.testframework.gametest.StructureTemplateBuilder;
 import sirttas.elementalcraft.block.ECBlocks;
 
@@ -12,6 +13,9 @@ public class RangeShrineUpgradeTemplates {
 
     private RangeShrineUpgradeTemplates() {}
 
+    public static final String HARVEST_SHRINE_WITH_1_RANGE_TEMPLATE_NAME = "elementalcraft:harvest_shrine_with_1_range";
+
+    @RegisterStructureTemplate(HARVEST_SHRINE_WITH_1_RANGE_TEMPLATE_NAME)
     public static final Supplier<StructureTemplateBuilder> HARVEST_SHRINE_WITH_1_RANGE_TEMPLATE = Lazy.of(() -> StructureTemplateBuilder.withSize(1, 2, 1)
             .set(0, 1, 0, ECBlocks.HARVEST_SHRINE.get().defaultBlockState())
             .set(0, 0, 0, ECBlocks.RANGE_SHRINE_UPGRADE.get().defaultBlockState().setValue(BlockStateProperties.FACING, Direction.UP)));
